8f12596e58e7988f7ff3618592e92189369cbd25
Lianuch <a.nyzhnyk@nltu.lviv.ua>
2025-01-10 21:41:25 +0200

    deploy project by docker-compose

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 
 import mongoose from "mongoose";
 import express from "express";
+import cors from "cors"
 import bodyParser from "body-parser";
 import { DB_URL, Port } from "./config/config";
 import { barberRouter } from "./routes/barbersRoutes";
@@ -8,7 +9,7 @@ import { barberRouter } from "./routes/barbersRoutes";
 const main = async () => {
   try {
     const app = express();
-
+    app.use(cors())
     await mongoose.connect(DB_URL);
     console.log("Connected to DB");
 
@@ -16,7 +17,7 @@ const main = async () => {
     app.use("/barbers", barberRouter);
 
     app.listen(Port, () => {
-      console.log("Server started on port 3000");
+      console.log(`Server started on port ${Port}`);
     });
   } catch (e) {
     console.log(e);
--- server/config/config.ts
+++ server/config/config.ts
@@ -1,7 +1,8 @@
 
-const Port = 3000
+const Port = 5000
 const  Db_Name = "barbershop"
 
-const DB_URL = `mongodb://127.0.0.1/${Db_Name}`
+// const DB_URL = `mongodb://mongo:127.0.0.1/${Db_Name}`
+const DB_URL = `mongodb://mongo:27017/${Db_Name}`;
 
 export {Port, DB_URL}
